0a854b5af8627ca03276626f078cd3151ddcf51f
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-10-18 12:10:56 +0300

    Update sketch_oct10a.ino

--- sketch_oct10a.ino
+++ sketch_oct10a.ino
@@ -1,8 +1,11 @@
 int ledPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 int numLeds = 10;
-long long itc_bin_num(long long number) {
-    int a = 0, b = 1;
-    while (number) {
+
+long long itc_bin_num(int number)
+{
+    long long a = 0, b = 1;
+    while (number)
+    {
         a += number % 2 * b;
         number /= 2;
         b *= 10;
@@ -10,26 +13,33 @@ long long itc_bin_num(long long number) {
     return a;
 }
 
-void setup() {
+void setup()
+{
     Serial.begin(9600);
-    for (int i = 0; i < numLeds; i++) {
+    for (int i = 0; i < numLeds; i++) 
+    {
         pinMode(ledPins[i], OUTPUT);
     }
 }
 
-void loop() {
-    int number = Serial.parseInt();
-    int numbin = itc_bin_num(number);
-    while(numbin > 0) {
-        int s = numbin % 10;
-        for (int i = 0; i < numLeds; i++) 
+void loop()
+{
+      while(Serial.available() > 0)
+      {
+      int number = Serial.parseInt();
+      if(number != 0){
+      long long numbin = itc_bin_num(number);
+      for(int i = 0; i < 10; i++){
+        int d= numbin % 10;
+        if(d == 1)
         {
-            if (s == 1) {
-                digitalWrite(ledPins[i], HIGH);
-            } else {
-                digitalWrite(ledPins[i], LOW);
-            }
-            numbin /= 10;
+          digitalWrite(ledPins[i], HIGH);
         }
-    }
-}
+        else{
+          digitalWrite(ledPins[i], LOW);
+        }
+        numbin /= 10;
+      }  
+      }
+      }    
+  }
